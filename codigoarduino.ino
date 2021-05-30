6ea14c4ded6b38e52eabd35985930374247df360
Alismos <46933082+Alismos@users.noreply.github.com>
2021-05-30 01:48:48 -0500

    Add files via upload

--- codigoarduino.ino
+++ codigoarduino.ino
@@ -4,20 +4,29 @@
 #include <HTTPClient.h>
 #include <WiFi.h>
 
+//Información de conexión WiFi
 const char* ssid = "NCESCOBAR";
 const char* password = "NoracanoM43602553";
-
 const char* host = "192.168.1.5";
 const int port = 80;
 const int watchdog = 5000;
 unsigned long previousMillis = millis();
 
+//Pin donde esta conectado el led
+int led = 2;
+
+
 void setup()
 {
   delay(10);
   Serial.begin(115200);
-  WiFi.begin(ssid, password);
 
+  pinMode(led, OUTPUT); //Declaramos el pin del led como saldia
+  digitalWrite(led, LOW); //Iniciamos con el led apagado
+
+  //Iniciamos la conexión WiFi
+
+  WiFi.begin(ssid, password);
   Serial.print("Conectando...");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -35,19 +44,19 @@ void setup()
 }
 
 void loop() {
-   /* float h = random(1,100);
-    float t = random(1,100);
-    Serial.print("Humedad = ");
-    Serial.print(h);
-    Serial.print("%  ");
-    Serial.print("Temperatura = ");
-    Serial.print(t);
-    Serial.println("°C  ");
-    delay(800);
+  float h = random(1, 100);
+  float t = random(1, 100);
+  Serial.print("Humedad = ");
+  Serial.print(h);
+  Serial.print("%  ");
+  Serial.print("Temperatura = ");
+  Serial.print(t);
+  Serial.println("°C  ");
+  delay(800);
 
-    unsigned long currentMillis = millis();
+  unsigned long currentMillis = millis();
 
-    if ( currentMillis - previousMillis > watchdog ) {
+  if ( currentMillis - previousMillis > watchdog ) {
     previousMillis = currentMillis;
     WiFiClient client;
 
@@ -62,9 +71,26 @@ void loop() {
     url += h;
 
     client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" +
-               "Connection: close\r\n\r\n");
+                 "Host: " + host + "\r\n" +
+                 "Connection: close\r\n\r\n");
     unsigned long timeout = millis();
+    delay(500);
+
+    if (client.available()) {
+      // Leemos la primer línea de la solicitud y la guardamos en la variable string request
+      String request = client.readStringUntil('\r');
+      Serial.println(request); //Imprimimos la solicitud
+      client.flush(); //Descartamos los datos que se han escrito en el cliente y no se han leído
+
+      // Relacionamos la solicitud
+      if (request.indexOf("/LED=OFF") != -1) {
+        digitalWrite(ledPin, LOW);
+      }
+      if (request.indexOf("/LED=ON") != -1) {
+        digitalWrite(ledPin, HIGH);
+      }
+    }
+    
     while (client.available() == 0) {
       if (millis() - timeout > 5000) {
         Serial.println(">>> Client Timeout !");
@@ -72,5 +98,6 @@ void loop() {
         return;
       }
     }
-    }*/
+
+  }
 }
