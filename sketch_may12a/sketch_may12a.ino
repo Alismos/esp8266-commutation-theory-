9f08cb1df5a9a99552c61b014873b0ad02d1888b
jcamiloif6 <46933022+jcamiloif6@users.noreply.github.com>
2021-06-02 17:05:50 -0500

    Add files via upload

--- sketch_may12a/sketch_may12a.ino
+++ sketch_may12a/sketch_may12a.ino
@@ -1,134 +1,109 @@
-#include <dummy.h>
-#include <Arduino.h>
-
-#include <HTTPClient.h>
 #include <WiFi.h>
 
-//Información de conexión WiFi
-const char* ssid = "Amobladosdh711";
-const char* password = "A30E8B7AC43477EB";
-const char* host = "192.168.0.16";
-const int port = 80;
-const int watchdog = 5000;
-unsigned long previousMillis = millis();
-
-//Pin donde esta conectado el led
-int led = 2;
+//------------------Servidor Web en puerto 80---------------------
 
 WiFiServer server(80);
 
-void setup()
-{
-  delay(10);
+//---------------------Credenciales de WiFi-----------------------
+
+const char* ssid     = "Amobladosdh711";
+const char* password = "A30E8B7AC43477EB";
+
+//---------------------VARIABLES GLOBALES-------------------------
+const int LED1 = 13;
+const int LED2 = 12;
+
+char linebuf[80];
+int charcount = 80;
+
+
+
+
+//---------------------------SETUP--------------------------------
+void setup() {
   Serial.begin(115200);
 
-  pinMode(led, OUTPUT); //Declaramos el pin del led como saldia
-  digitalWrite(led, LOW); //Iniciamos con el led apagado
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
 
-  //Iniciamos la conexión WiFi
-
+  Serial.printf("Conectando a: %s\n", ssid);
+  // Conexión WIFI
   WiFi.begin(ssid, password);
-  Serial.print("Conectando...");
+  //Cuenta hasta 50 si no se puede conectar lo cancela
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+    Serial.println("Conectando... ");
+    delay(2000);
   }
 
-  Serial.println("");
-  Serial.println("WiFi connected");
-
-  server.begin();
-  Serial.println("Server started");
-
-  Serial.println("IP address: ");
-  Serial.print("http://");
+  Serial.println("Conectado. ");
+  Serial.print("Dirección IP del estado: ");
   Serial.print(WiFi.localIP());
-  Serial.println("/");
-
-  Serial.println("Humedad y Temperatura\n\n");
-
+  server.begin();
 }
 
 void loop() {
-  float h = random(1, 100);
-  float t = random(1, 100);
-  Serial.print("Humedad = ");
-  Serial.print(h);
-  Serial.print("%  ");
-  Serial.print("Temperatura = ");
-  Serial.print(t);
-  Serial.println("°C  ");
-  delay(800);
-
-  unsigned long currentMillis = millis();
-
-  if ( currentMillis - previousMillis > watchdog ) {
-    previousMillis = currentMillis;
-    WiFiClient client = server.available();
-
-    /*if (!client.connect(host, port)) {
-      Serial.println("Fallo al conectar");
-      return;
-      }*/
-
-    if (!client) {
-      return;
+  WiFiClient client = server.available();
+  if (client) { // If a new client connects,
+    Serial.println("Nuevo Cliente"); // print a message out in the serial port
+    memset(linebuf,0,sizeof(linebuf));
+    charcount=0;
+    boolean currentLineIsBlank=true;
+    
+    while (client.connected()) { // loop while the client's connected
+      if (client.available()) { // if there's bytes to read from the client,
+        char c = client.read(); // read a byte, then
+        Serial.write(c); // print it out the serial monitor
+        linebuf[charcount]=c;
+        if (charcount<sizeof(linebuf)-1) charcount++; // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          
+          if (c == '\n' && currentLineIsBlank) {
+          // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+// and a content-type so the client knows what's coming, then a blank line:
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-type:text/html");
+          client.println("Connection: close");
+          client.println();
+          client.println("<!DOCTYPE HTML><html><head>"); 
+          client.println("<body style=\"background-color:#0066aa:\">");
+          client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"></head>");
+          client.println("<h1><style=\"width:500px;height:20px;color:#ffffff;\"> Tutorial Web Server ESP32 </h1>");
+          client.println("<p style=\"color:#ffffff\"> LED #1 <a href=\"on1\"><button>ON</button></a>&nbsp;<a href=\"off1\"><button>OFF</button><a/></p>");
+          client.println("<p style=\"color:#ffffff\"> LED #2 <a href=\"on2\"><button>ON</button></a>&nbsp;<a href=\"off2\"><button>OFF</button><a/></p>");
+          client.println("</html>");
+          break;
+          }
+          
+          // turns the GPIOs on and off
+          if (c == '\n') {
+            currentLineIsBlank = true;
+          if(strstr(linebuf,"GET /on1")>0){
+            Serial.println("LED 1 ON");
+            digitalWrite(LED1, HIGH);
+          }
+          else if(strstr(linebuf,"GET /off1")>0){
+            Serial.println("LED 1 OFF");
+            digitalWrite(LED1, LOW);
+          }
+          else if(strstr(linebuf,"GET /on2")>0){
+            Serial.println("LED 2 ON");
+            digitalWrite(LED2, HIGH);
+          } 
+          else if(strstr(linebuf,"GET /off2")>0){
+            Serial.println("LED 2 OFF");
+            digitalWrite(LED2, LOW);
+          }
+          currentLineIsBlank = true;
+          memset(linebuf,0,sizeof(linebuf));
+          charcount=0;
+        } else if (c != '\r'){
+            currentLineIsBlank = false;
+          }
+        }
     }
-
-    // Wait until the client sends some data
-    Serial.println("new client");
-    while (!client.available()) {
-      delay(1);
-    }
-/*
-    String url = "/TutorialESP8266/info.php?temp=";
-    url += t;
-    url += "&hum=";
-    url += h;
-
-    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-                 "Host: " + host + "\r\n" +
-                 "Connection: close\r\n\r\n");
-    unsigned long timeout = millis();
-    delay(500);
-
-*/
-    // Leemos la primer línea de la solicitud y la guardamos en la variable string request
-    String request = client.readStringUntil('\r');
-    Serial.println(request); //Imprimimos la solicitud
-    client.flush(); //Descartamos los datos que se han escrito en el cliente y no se han leído
-
-    // Relacionamos la solicitud
-    int value = LOW;
-    if (request.indexOf("/LED=OFF") != -1) {
-      Serial.println("1");
-      value = HIGH;
-    }
-    if (request.indexOf("/LED=ON") != -1) {
-      value = LOW;
-    }
-
-    client.println("HTTP/1.1 200 OK");
-    client.println("Content-Type: text/html");
-    client.println(""); //  do not forget this one
-    client.println("<!DOCTYPE HTML>");
-    client.println("<html>");
-
-    client.print("Led pin is now: ");
-
-      if (value == HIGH) {
-      client.print("On");
-    } else {
-      client.print("Off");
-    }
-    client.println("<br><br>");
-    client.println("<a href=\"/LED=ON\"\"><button>Turn On </button></a>");
-    client.println("<a href=\"/LED=OFF\"\"><button>Turn Off </button></a><br />");
-    client.println("</html>");
-
     delay(1);
-    Serial.println("Client disonnected");
-    Serial.println("");
-
+    client.stop();
+    Serial.println("Cliente desconectado");
   }
 }
